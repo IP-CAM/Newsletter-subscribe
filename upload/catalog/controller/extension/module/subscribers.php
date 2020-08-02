42485076ae8c165b03029dad232f736eb1490ee7
invays <ayan.paul1234@yandex.ru>
2020-08-02 15:34:04 +0300

    Added privacy policy

--- upload/catalog/controller/extension/module/subscribers.php
+++ upload/catalog/controller/extension/module/subscribers.php
@@ -4,17 +4,24 @@ class ControllerExtensionModuleSubscribers extends Controller{
 		$this->load->language('extension/module/subscribers');
 
 		$data['heading_title'] = $this->language->get('heading_title');
-
+		
+		$this->load->model('catalog/information');
+		$information_info = $this->model_catalog_information->getInformation($this->config->get('module_subscribers_agree'));
+		$data['text_link'] = $this->url->link('information/information', 'information_id=' . $this->config->get('module_subscribers_agree'));
+		$data['title'] = $information_info['title'];
+		$data['text_agreer'] = sprintf($this->language->get('entry_submit'));
+		
 		return $this->load->view('extension/module/subscribers', $data);
 	}
 
 	public function newSubscribe(){
 		$this->load->model('extension/module/subscribers');
-
+		
 		$json = array();
 		$json['message'] = $this->model_extension_module_subscribers->Subscribe($this->request->post);
 
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 	}
+	
 }
